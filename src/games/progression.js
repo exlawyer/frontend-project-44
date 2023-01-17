2d9a9d8e69907f810b618c75ba243404dbdc7637
exlawyer <i@eaksenov.ru>
2023-01-17 17:20:01 +0300

    fix loop issue

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,20 +7,20 @@ const progression = () => {
   const result = () => {
     const progrStep = Math.floor(Math.random() * 20);
     const numbers = [];
-    numbers.length = Math.floor(Math.random() * (20 - 5) + 5);
+    const arrLength = Math.floor(Math.random() * (20 - 5) + 5);
     let firstNum = Math.floor(Math.random() * 50 + 1);
-    for (const number of numbers) {
+    for (let i = 0; i <= arrLength; i += 1) {
       numbers.push(firstNum);
       firstNum += progrStep;
     }
     const numberToHide = Math.floor(Math.random() * numbers.length);
     const correctAnswer = numbers[numberToHide].toString();
-     numbers[numberToHide] = '..';
-     numbers.join(' ');
-     const expression = numbers.toString();
-     return [expression, correctAnswer];
-    };     
-  gameTemplate(toDo, result);
+    numbers[numberToHide] = '..';
+    numbers.join(' ');
+    const expression = numbers.toString();
+    return [expression, correctAnswer];
   };
+  gameTemplate(toDo, result);
+};
 
-  export default progression;
+export default progression;
